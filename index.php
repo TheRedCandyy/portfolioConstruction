72b448650462bf3ee23eb8d56f72b4600615b12d
Alexandre Tavares <66254675+TheRedCandyy@users.noreply.github.com>
2021-08-29 12:21:36 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ include("includes/head.php");
 ?>
     <body>
         <div class="container vh-100 center-vertical">
-            <h1 class="display-1">Hi, I'm <mark>Alex</mark> :)!</h1><br><br>
+            <h1 class="display-1">Hi, I'm <mark>Alex</mark>!</h1><br><br>
             <h1 class="display-5">I am currently developing my new portfolio.</h1><br>
             <p>Come back later and u might see it finished. Meanwhile you can still contact me via <a href="https://www.linkedin.com/in/alexandre-tavares-15082a194/">Linkedin</a> !</p>
         </div>
